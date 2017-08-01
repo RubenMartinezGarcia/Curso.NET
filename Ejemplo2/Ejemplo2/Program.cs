b143569a9f436d2fde618a1397ae3fab2bc7c640
Rubén <rubmargar93@gmail.com>
2017-08-01 12:07:30 +0200

    Ejemplo 2

---
+++ Ejemplo2/Ejemplo2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var importe = 133;
+            var result = 0;
+            var valid = Change(importe, out result);
+            if (!valid)
+            {
+                throw new Exception("importe invalido");
+            }
+            Console.ReadLine();
+
+        }
+
+        static bool Change(int importe, out int change)
+        {
+            var result = true;
+            switch (importe)
+            {
+                case 500:
+                    {
+                        change = 75;
+                        return result;
+                    }
+                case 200:
+                    {
+                        change = 50;
+                        return result;
+                    }
+                case 100:
+                    {
+                        change = 30;
+                        return result;
+                    }
+                default:
+                    {
+                        change = 0;
+                        return result;
+                    }
+            }
+            change = 0;
+            return result;
+        }
+    }
+}
