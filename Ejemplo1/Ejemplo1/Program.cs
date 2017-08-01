7ba8c8f9c561dc8067540d6326c8b000b09fb076
Rubén <rubmargar93@gmail.com>
2017-08-01 12:04:16 +0200

    Ejemplo 1

---
+++ Ejemplo1/Ejemplo1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cadena = "Hello";
+            for (int i = 0; i < 1000000; i++)
+            {
+                cadena = cadena + i.ToString();
+            }
+            Console.WriteLine(cadena);
+            Console.ReadLine();
+
+            var sb = new StringBuilder();
+            sb.Append("Hello");
+            for (int i = 0; i < 1000000; i++)
+            {
+                sb.Append(i);
+            }
+        }
+    }
+}
