60c4a3d89aad9df2a601025283ea2f7c7948de92
Rubén <rubmargar93@gmail.com>
2017-08-02 13:50:10 +0200

    Pruebas varias

---
+++ Pruebas/Pruebas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pruebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public void Save()
+        {
+
+        }
+    }
+}
