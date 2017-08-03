383ced00d5ed0a0b3a9a6e0eb453885e4a454ed3
Rubén <rubmargar93@gmail.com>
2017-08-03 11:00:44 +0200

    Ejemplo 6

---
+++ Ejemplo6/Ejemplo6/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ticket = new Ticket() { Total = 1000 };
+            var repositoryTicket = new RepositoryTicket();
+            repositoryTicket.Add(ticket);
+        }
+    }
+
+    public abstract class EntityBase
+    {
+        public Guid Id { get; set; }
+        public static Guid CreateIdentifier()
+        {
+            return new Guid();
+        }
+    }
+
+    public class Ticket : EntityBase
+    {
+        public Decimal Total { get; set; }
+    }
+    public class Person : EntityBase
+    {
+        public string Name { get; set; }
+    }
+    public class RepositoryTicket:RepositoryGeneric<Ticket>
+    {
+        
+    }
+    public class RepositoryPerson:RepositoryGeneric<Person>
+    {
+        
+    }
+    public class RepositoryGeneric<T> where T : EntityBase
+    {
+        public virtual void Add(T entity)
+        {
+            entity.Id = EntityBase.CreateIdentifier();
+        }
+    }
+}
