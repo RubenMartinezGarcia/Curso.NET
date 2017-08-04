887054090dc5f1a4492a73f328b63735aa85e998
Rubén <rubmargar93@gmail.com>
2017-08-04 12:04:10 +0200

    Nullable

---
+++ Ejemplo8/Ejemplo8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? a = null;
+            //int b = null;
+
+            System.Nullable<int> c = null;
+        }
+    }
+}
